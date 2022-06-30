946d30d4ba14ca06ae8d0f16666fb6b8304d146f
sstarodubov <s.starodubov@litebox.ru>
2022-06-30 12:35:24 +0400

    Cleanup

--- Euler/inProgress/092.py
+++
@@ -1,9 +0,0 @@
-def checkSeq(number=0):
-    # count = 0
-    # number_start = number
-    while number!=89 and number!=1:
-        number = sum([int(i)*int(i) for i in str(number)])
-        # count += 1
-    return number
-
-print([checkSeq(i) for i in range(1, 1000)])
