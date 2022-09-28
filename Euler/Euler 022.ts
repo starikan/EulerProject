ffe5a24725702b11955dae0afb23a9fb4aa0cf91
sstarodubov <s.starodubov@litebox.ru>
2022-09-28 10:55:56 +0400

    Euler 21,22

---
+++ Euler/Euler 022.ts
@@ -0,0 +1,57 @@
+/**
+Using names.txt (right click and 'Save Link/Target As...'), a 46K text file containing over five-thousand first names,
+begin by sorting it into alphabetical order. Then working out the alphabetical value for each name, multiply this value
+by its alphabetical position in the list to obtain a name score.
+
+For example, when the list is sorted into alphabetical order, COLIN, which is worth 3 + 15 + 12 + 9 + 14 = 53,
+is the 938th name in the list. So, COLIN would obtain a score of 938 × 53 = 49714.
+
+What is the total of all the name scores in the file?
+ */
+
+import fs from 'fs';
+import path from 'path';
+
+const ALPHABET: Record<string, number> = {
+  A: 1,
+  B: 2,
+  C: 3,
+  D: 4,
+  E: 5,
+  F: 6,
+  G: 7,
+  H: 8,
+  I: 9,
+  J: 10,
+  K: 11,
+  L: 12,
+  M: 13,
+  N: 14,
+  O: 15,
+  P: 16,
+  Q: 17,
+  R: 18,
+  S: 19,
+  T: 20,
+  U: 21,
+  V: 22,
+  W: 23,
+  X: 24,
+  Y: 25,
+  Z: 26,
+};
+
+const aToN = (v: string): number => {
+  const numbers = v
+    .toUpperCase()
+    .split('')
+    .map((a: keyof typeof ALPHABET) => ALPHABET[a] || 0);
+  return numbers.reduce((s, n) => s + n, 0);
+};
+
+const rawData = fs.readFileSync(path.join(__dirname, 'assets', 'Euler_022.txt')).toString();
+
+const names = rawData.replace(/\"/g, '').split(',').sort();
+const digits = names.map((v) => aToN(v));
+const result = digits.map((v, i) => v * (i + 1)).reduce((s, n) => s + n, 0);
+console.log(result);
